5260bf76aebc27dc3583dc87ed919da84cc55b30
Isa <bartkowiak.isadora@gmail.com>
2018-09-16 13:35:26 +0200

    Repair chrome for travis CI tests

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeCanary'],
+    customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+    },
     singleRun: false
   });
 };
