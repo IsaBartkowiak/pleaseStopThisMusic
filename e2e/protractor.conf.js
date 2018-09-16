b49b69f2af4ac8df0431b1b8114873e1bc943c3f
Isa <bartkowiak.isadora@gmail.com>
2018-09-16 15:22:20 +0200

    Fix e2e chrome for travis

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -6,23 +6,26 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+  './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+    browserName: 'chrome',
+    chromeOptions: {
+     args: [ "--headless", "--disable-gpu" ]
+   }
+ },
+ directConnect: true,
+ baseUrl: 'http://localhost:4200/',
+ framework: 'jasmine',
+ jasmineNodeOpts: {
+  showColors: true,
+  defaultTimeoutInterval: 30000,
+  print: function() {}
+},
+onPrepare() {
+  require('ts-node').register({
+    project: require('path').join(__dirname, './tsconfig.e2e.json')
+  });
+  jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+}
 };
