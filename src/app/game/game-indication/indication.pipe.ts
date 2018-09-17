efb416b75605de8151e9b6070ec17f3e0e1cf936
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 18:53:49 +0200

    Create indication pipe

--- src/app/game/game-indication/indication.pipe.ts
+++ src/app/game/game-indication/indication.pipe.ts
@@ -5,8 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IndicationPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  transform(distance: number): any {
+		if(!distance) return;
+		
+		if(distance < 30){
+			return "Burning";
+		}
+		if(distance < 100){
+			return "Very hot";
+		}
+		if(distance < 200){
+			return "Hot";
+		}
+		if(distance < 300){
+			return "Tepid";
+		}
+		if(distance < 400){
+			return "Cold";
+		}
+		if(distance < 500){
+			return "Very cold";
+		}
+		if(distance < 600){
+			return "Glacial";
+		}
+	}
 
 }
