b0641c266d18b2f9b612cdc5e0d700f6f699586f
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 19:24:30 +0200

    Fix lint issues

--- src/app/game/game-container/game-container.component.spec.ts
+++ src/app/game/game-container/game-container.component.spec.ts
@@ -2,8 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable, Subject} from 'rxjs';
 import { GameContainerComponent } from './game-container.component';
 import { MusicPlayerComponent } from '../music-player/music-player.component';
-import { By }              from '@angular/platform-browser';
-import { DebugElement }    from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('GameContainerComponent', () => {
   let component: GameContainerComponent;
@@ -12,79 +12,79 @@ describe('GameContainerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ GameContainerComponent, MusicPlayerComponent ]
-    })
+  })
     .compileComponents();
-  }));
+}));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GameContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+});
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-  
+});
+
   it('should have all properties', () => {
-    expect(typeof component.randomPosition == "object").toBeTruthy("randomPosition is set");
-    expect(component.music).toBeTruthy("musics is set");
-    expect(component.music.name).toBe("Khaled - Aïcha", "Music name is set");
-    expect(component.win instanceof Subject).toBeTruthy("win is set");
-    expect(component.try instanceof Subject).toBeTruthy("try is set");
+    expect(typeof component.randomPosition === 'object').toBeTruthy('randomPosition is set');
+    expect(component.music).toBeTruthy('musics is set');
+    expect(component.music.name).toBe('Khaled - Aïcha', 'Music name is set');
+    expect(component.win instanceof Subject).toBeTruthy('win is set');
+    expect(component.try instanceof Subject).toBeTruthy('try is set');
     expect(component.totalMusic).toBeTruthy();
-  });
-  
+});
+
   it('should have answer div on page', () => {
-    let ansDiv = fixture.debugElement.query(By.css("#answer"));
+    const ansDiv = fixture.debugElement.query(By.css('#answer'));
     expect(ansDiv).toBeTruthy();
-  });
-  
+});
+
   it('should random position answer between right values', () => {
-    let top = parseInt(component.randomPosition.top.slice(0, -1));
-    let left = parseInt(component.randomPosition.left.slice(0, -1));
-    let ansDiv = <HTMLElement> document.getElementById("answer");
+    const top = parseInt(component.randomPosition.top.slice(0, -1), 10);
+    const left = parseInt(component.randomPosition.left.slice(0, -1), 10);
+    const ansDiv = <HTMLElement> document.getElementById('answer');
     expect(top >= 0 && top <= (window.innerHeight - ansDiv.clientHeight)).toBeTruthy();
     expect(left >= 0 && left <= (window.innerWidth - ansDiv.clientWidth)).toBeTruthy();
-  });
-  
+});
+
   it('calculateDistance() should return the right distance', () => {
     component.randomPosition = {
-      'top': "100px", 
-      'left' : "100px"
-    }
-    let ansDiv = <HTMLElement> document.getElementById("answer");
-    ansDiv.style.width = "0px";
-    ansDiv.style.height = "0px";
-    ansDiv.style.lineHeight = "0px";
-    fixture.detectChanges();
-    
-    let distance = component.calculateDistance(0,0);
-    expect(distance).toBe(141, 'Calculate right distance click'); //diagonale
-  });
-  
+      'top': '100px',
+      'left' : '100px'
+  };
+  const ansDiv = <HTMLElement> document.getElementById('answer');
+  ansDiv.style.width = '0px';
+  ansDiv.style.height = '0px';
+  ansDiv.style.lineHeight = '0px';
+  fixture.detectChanges();
+
+  const distance = component.calculateDistance(0, 0);
+    expect(distance).toBe(141, 'Calculate right distance click'); // diagonale
+});
+
   it('should show answer div when clicked on the right place', () => {
     component.ngOnInit();
     component.randomPosition = {
-      'top': "0px", 
-      'left' : "0px"
-    }
-    fixture.detectChanges();
-    let container = <HTMLElement> document.querySelector(".game-container");
-    container.click();
-    let distance = component.calculateDistance(1,1);
-    expect(distance).toBe(0, 'Return 0 if answer is clicked');
-    expect(component.finded).toBe(true);
-  });
-  
+      'top': '0px',
+      'left' : '0px'
+  };
+  fixture.detectChanges();
+  const container = <HTMLElement> document.querySelector('.game-container');
+  container.click();
+  const distance = component.calculateDistance(1, 1);
+  expect(distance).toBe(0, 'Return 0 if answer is clicked');
+  expect(component.finded).toBe(true);
+});
+
   it('should can pass to the next music', () => {
-    let tmpMusicName = component.music.name;
-    let tmpTopPosition = component.randomPosition.top;
+    const tmpMusicName = component.music.name;
+    const tmpTopPosition = component.randomPosition.top;
     component.nextMusic();
     fixture.detectChanges();
     expect(component.music).toBeTruthy();
-    expect(tmpMusicName != component.music.name).toBeTruthy();
-    expect(tmpTopPosition != component.randomPosition.top).toBeTruthy();
-  });
+    expect(tmpMusicName !== component.music.name).toBeTruthy();
+    expect(tmpTopPosition !== component.randomPosition.top).toBeTruthy();
+});
 
 });
--- src/app/game/game-container/game-container.component.ts
+++ src/app/game/game-container/game-container.component.ts
@@ -1,87 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable, Subject} from 'rxjs';
 import { MusicService } from './../shared/music.service';
 
 @Component({
-	selector: 'pstm-game-container',
-	templateUrl: './game-container.component.html',
-	styleUrls: ['./game-container.component.scss']
+    selector: 'pstm-game-container',
+    templateUrl: './game-container.component.html',
+    styleUrls: ['./game-container.component.scss']
 })
-export class GameContainerComponent implements OnInit {
+export class GameContainerComponent implements OnInit, OnDestroy {
 
-	music : any;
-	try : Subject<any> = new Subject();
-	win : Subject<any> = new Subject();
-	finded : boolean = false;
-	end : boolean = false;
-	clickX : number;
-	clickY: number;
-	showCircle: boolean = false;
-	randomPosition: any;
-	totalMusic: number;	
+    music: any;
+    try: Subject<any> = new Subject();
+    win: Subject<any> = new Subject();
+    finded = false;
+    end = false;
+    clickX: number;
+    clickY: number;
+    showCircle = false;
+    randomPosition: any;
+    totalMusic: number;
 
-	constructor(private MusicService: MusicService) { }
+    constructor (private musicService: MusicService) { }
 
-	ngOnInit() {
-		this.setRandomPosition();
-		this.MusicService.getCurrentMusic().subscribe(music => {
-			this.music = music;
-		});
-		this.totalMusic = this.MusicService.getTotalCount();
-	}
-	
-	setRandomPosition(){
-		this.randomPosition = {
-			'top': Math.floor(Math.random() * 86) + "%", 
-			'left' : Math.floor(Math.random() * 86) + "%"
-		};
-	}
-	
-	nextMusic(){
-		this.finded = false;
-		this.setRandomPosition();
-		if(!this.MusicService.nextMusic()){
-			this.end = true;
-		}
-	}
+    ngOnInit() {
+        this.setRandomPosition();
+        this.musicService.getCurrentMusic().subscribe(music => {
+            this.music = music;
+        });
+        this.motalMusic = this.MusicService.getTotalCount();
+    }
 
-	checkResult(event){
-		this.clickX = event.clientX;
-		this.clickY = event.clientY;
-		this.showCircle = true;
-		let that = this;
-		setTimeout(function(){
-			that.showCircle = false;
-		}, 500)
-		const distance = this.calculateDistance(event.clientX, event.clientY);
-		this.notifyTry(distance);
-		if(distance === 0){
-			this.finded = true;
-			this.notifyWin(true);
-		}
-	}
-	
-	calculateDistance(clickX, clickY){
-		const answer = document.getElementById('answer');
-		if(
-			clickX >= answer.offsetLeft && clickX <= (answer.offsetLeft + answer.offsetWidth) &&
-			clickY >= answer.offsetTop && clickY <= (answer.offsetTop + answer.offsetHeight)
-		){	
-			return 0;
-		}
-		return Math.floor(Math.sqrt(Math.pow(clickX - (answer.offsetLeft+(answer.offsetWidth/2)), 2) + Math.pow(clickY - (answer.offsetTop+(answer.offsetHeight/2)), 2)));
-	}
-	
-	notifyTry(distance) {
-		this.try.next(distance);
-	}
-	
-	notifyWin(status) {
-		this.win.next(status);
-	}
-	
-	ngOnDestroy(){
-		this.MusicService.getCurrentMusic().unsubscribe();
-	}
+    setRandomPosition() {
+        this.randomPosition = {
+            'top': Math.floor(Math.random() * 86) + '%',
+            'left' : Math.floor(Math.random() * 86) + '%'
+        };
+    }
+
+    nextMusic() {
+        this.finded = false;
+        this.setRandomPosition();
+        if (!tmis.MusicService.nextMusic()) {
+            this.end = true;
+        }
+    }
+
+    checkResult(event) {
+        this.clickX = event.clientX;
+        this.clickY = event.clientY;
+        this.showCircle = true;
+        const that = this;
+        setTimeout(function() {
+            that.showCircle = false;
+        }, 500);
+        const distance = this.calculateDistance(event.clientX, event.clientY);
+        this.notifyTry(distance);
+        if (distance === 0) {
+            this.finded = true;
+            this.notifyWin(true);
+        }
+    }
+
+    calculateDistance(clickX, clickY) {
+        const answer = document.getElementById('answer');
+        if (
+            clickX >= answer.offsetLeft &&
+            clickX <= (answer.offsetLeft + answer.offsetWidth) &&
+            clickY >= answer.offsetTop &&
+            clickY <= (answer.offsetTop + answer.offsetHeight)) {
+            return 0;
+        }
+        return Math.floor(Math.sqrt(Math.pow(
+            clickX - (answer.offsetLeft + (answer.offsetWidth / 2)), 2) +
+            Math.pow(clickY - (answer.offsetTop + (answer.offsetHeight / 2)), 2
+        )));
+    }
+
+    notifyTry(distance) {
+      this.try.next(distance);
+  }
+
+  notifyWin(status) {
+      this.win.next(status);
+  }
+
+  ngOnDestroy() {
+      this.musicService.getCurrentMusic().unsubscribe();
+  }
 
 }
