0f60708fb43414c3384e348e57206e43cbc58a90
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 18:47:13 +0200

    Init game indication pipe

---
+++ src/app/game/game-indication/indication.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { IndicationPipe } from './indication.pipe';
+
+describe('IndicationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new IndicationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
