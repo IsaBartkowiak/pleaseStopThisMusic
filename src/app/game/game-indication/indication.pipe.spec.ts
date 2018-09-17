ed302e29a1ec586de997ee52088ba3bd0706746e
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 19:05:25 +0200

    Fix lint error on game indication pipe

--- src/app/game/game-indication/indication.pipe.spec.ts
+++ src/app/game/game-indication/indication.pipe.spec.ts
@@ -5,36 +5,36 @@ describe('IndicationPipe', () => {
     const pipe = new IndicationPipe();
     expect(pipe).toBeTruthy();
 });
-  
+
   it('should return if null value is passed', () => {
     const pipe = new IndicationPipe();
     expect(pipe.transform(null)).toBeFalsy();
 });
-  
+
   it('should return BURNING if distance is < 30', () => {
     const pipe = new IndicationPipe();
-    expect(pipe.transform(29)).toBe("Burning");
+    expect(pipe.transform(29)).toBe('Burning');
 });
-  
+
   it('should return GLACIAL if distance is > 500', () => {
     const pipe = new IndicationPipe();
-    expect(pipe.transform(550)).toBe("Glacial");
+    expect(pipe.transform(550)).toBe('Glacial');
 });
-  
+
   it('should return TEPID if distance is > 200', () => {
     const pipe = new IndicationPipe();
-    expect(pipe.transform(250)).toBe("Tepid");
+    expect(pipe.transform(250)).toBe('Tepid');
 });
-  
+
   it('should stop if wrong format', () => {
     const pipe = new IndicationPipe();
     expect(pipe.transform(250, 'test')).toBeFalsy();
 });
-  
+
   it('should return good format', () => {
     const pipe = new IndicationPipe();
-    expect(pipe.transform(250, 'human')).toBe("Tepid");
-    expect(pipe.transform(250, 'class')).toBe("tepid");
+    expect(pipe.transform(250, 'human')).toBe('Tepid');
+    expect(pipe.transform(250, 'class')).toBe('tepid');
 });
-  
+
 });
--- src/app/game/game-indication/indication.pipe.ts
+++ src/app/game/game-indication/indication.pipe.ts
@@ -5,31 +5,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IndicationPipe implements PipeTransform {
 
-  transform(distance: number, format:string = "human"): any {
-		if (!distance) return;
-		if  (format != "human" && format != "class") return;
-		
-		if (distance < 30) {
-			return format === "human" ? "Burning" : "burning";
-		}
-		if (distance < 100) {
-			return format === "human" ? "Very hot" : "very-hot";
-		}
-		if (distance < 200) {
-			return format === "human" ? "Hot" : "hot";
-		}
-		if (distance < 300) {
-			return format === "human" ? "Tepid" : "tepid";
-		}
-		if (distance < 400) {
-			return format === "human" ? "Cold" : "cold";
-		}
-		if (distance < 500) {
-			return format === "human" ? "Very cold" : "very-cold";
-		}
-		if (distance < 600) {
-			return format === "human" ? "Glacial" : "glacial";
-		}
-	}
+  transform (distance: number, format: string = 'human'): any {
+    if (!distance) {
+        return;
+    }
+    if  (format !== 'human' && format !== 'class') {
+        return;
+    }
+    if (distance < 30) {
+        return format === 'human' ? 'Burning' : 'burning';
+    }
+    if (distance < 100) {
+        return format === 'human' ? 'Very hot' : 'very-hot';
+    }
+    if (distance < 200) {
+        return format === 'human' ? 'Hot' : 'hot';
+    }
+    if (distance < 300) {
+        return format === 'human' ? 'Tepid' : 'tepid';
+    }
+    if (distance < 400) {
+        return format === 'human' ? 'Cold' : 'cold';
+    }
+    if (distance < 500) {
+        return format === 'human' ? 'Very cold' : 'very-cold';
+    }
+    if (distance < 600) {
+        return format === 'human' ? 'Glacial' : 'glacial';
+    }
+}
 
 }
