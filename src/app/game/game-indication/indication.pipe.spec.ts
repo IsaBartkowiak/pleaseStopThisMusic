cbbefef3ec355770cd15557b387ef19af8d0247c
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 18:52:15 +0200

    Set pipe indication test cases

--- src/app/game/game-indication/indication.pipe.spec.ts
+++ src/app/game/game-indication/indication.pipe.spec.ts
@@ -4,5 +4,26 @@ describe('IndicationPipe', () => {
   it('create an instance', () => {
     const pipe = new IndicationPipe();
     expect(pipe).toBeTruthy();
-  });
+});
+  
+  it('should return if null value is passed', () => {
+    const pipe = new IndicationPipe();
+    expect(pipe.transform(null)).toBeFalsy();
+});
+  
+  it('should return BURNING if distance is < 30', () => {
+    const pipe = new IndicationPipe();
+    expect(pipe.transform(29)).toBe("Burning");
+});
+  
+  it('should return GLACIAL if distance is > 500', () => {
+    const pipe = new IndicationPipe();
+    expect(pipe.transform(550)).toBe("Glacial");
+});
+  
+    it('should return TEPID if distance is > 200', () => {
+    const pipe = new IndicationPipe();
+    expect(pipe.transform(250)).toBe("Tepid");
+});
+  
 });
