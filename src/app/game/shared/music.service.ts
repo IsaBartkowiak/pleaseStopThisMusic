3ebe4f1b4ee5259cc068e02e1da39f05b756b4c1
Isa <bartkowiak.isadora@gmail.com>
2018-09-17 17:18:14 +0200

    Fix errors to pass tests

--- src/app/game/shared/music.service.ts
+++ src/app/game/shared/music.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, Inject, Optional } from '@angular/core';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { MUSICS } from './mock-musics';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class MusicService {
-
-	constructor() { }
+	private currentMusic: BehaviorSubject<any>;
+	private currentIndex: number = 0;
+	private musics: any[];
+	
+	constructor(@Inject('testdata') @Optional() private testdata?: any[]) {
+		this.musics = testdata || MUSICS;
+		this.currentMusic = new BehaviorSubject(this.musics[0]);
+	}
 	
 	getMusics(): Observable<any[]> {
-		return of(MUSICS);
+		return of(this.musics);
 	}
+	
+	getCurrentMusic(): BehaviorSubject<any> {
+		return this.currentMusic;
+	}
+	
+	nextMusic() {
+		if(this.currentIndex == this.musics.length) return false;
+		this.currentIndex++;
+		this.currentMusic.next(this.musics[this.currentIndex]);
+	}
+	
+	getTotalCount(): number {
+		return this.musics.length;
+	}
+
 }
