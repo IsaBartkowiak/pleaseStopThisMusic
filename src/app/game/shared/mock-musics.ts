b33fd47a453cd4c7340fc0744a53775a09df715b
Isa <bartkowiak.isadora@gmail.com>
2018-09-21 16:52:21 +0200

    Add new musics data

--- src/app/game/shared/mock-musics.ts
+++ src/app/game/shared/mock-musics.ts
@@ -1,14 +1,47 @@
 export const MUSICS: any[] = [
-  { 
-    id: 1, 
-    name: 'Khaled - Aïcha',
-    file: 'khaled-aicha.mp3',
-    image: 'khaled-aicha.jpg'
-  },
-  { 
-    id: 2, 
-    name: 'Manau - La tribu de Dana',
-    file: 'manau-dana.mp3',
-    image: 'manau-dana.jpg'
-  }
+{
+    id: 2,
+    name: 'K-maro - Femme like you',
+    file: 'k-maro-femme-like-you.mp3',
+},
+{
+    id: 2,
+    name: 'Rick Astley - Never gonna give you up',
+    file: 'never-gonna.mp3',
+},
+{
+    id: 1,
+    name: 'Yannick - Ces soirées là',
+    file: 'soiree-la.mp3',
+},
+{
+    id: 1,
+    name: 'Céline Dion - Pour que tu m\'aimes encore',
+    file: 'celine-encore.mp3',
+},
+{
+    id: 2,
+    name: 'Dirty dancing - Time of my life',
+    file: 'time-of-my-life.mp3',
+},
+{
+    id: 2,
+    name: 'Larusso - Tu m\'oublieras',
+    file: 'larusso-oublieras.mp3',
+},
+{
+    id: 2,
+    name: 'Jean-Luc Lahaye - Femme',
+    file: 'jean-luc-femme.mp3',
+},
+{
+    id: 1,
+    name: 'King africa - Bomba',
+    file: 'bomba.mp3',
+},
+{
+    id: 2,
+    name: 'Motus - Générique',
+    file: 'motus-generique.mp3',
+}
 ];
