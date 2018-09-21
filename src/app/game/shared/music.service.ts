38e4373cde189e356b050796971c6c565c2d5d13
Isa <bartkowiak.isadora@gmail.com>
2018-09-21 16:50:33 +0200

    Add reset musics function on music service

--- src/app/game/shared/music.service.ts
+++ src/app/game/shared/music.service.ts
@@ -9,28 +9,33 @@ export class MusicService {
 	private currentMusic: BehaviorSubject<any>;
 	private currentIndex: number = 0;
 	private musics: any[];
-	
+
 	constructor(@Inject('testdata') @Optional() private testdata?: any[]) {
 		this.musics = testdata || MUSICS;
 		this.currentMusic = new BehaviorSubject(this.musics[0]);
 	}
-	
+
 	getMusics(): Observable<any[]> {
 		return of(this.musics);
 	}
-	
+
 	getCurrentMusic(): BehaviorSubject<any> {
 		return this.currentMusic;
 	}
-	
+
 	nextMusic() {
-		if(this.currentIndex == this.musics.length) return false;
+		if(this.currentIndex == (this.musics.length - 1)) return false;
 		this.currentIndex++;
 		this.currentMusic.next(this.musics[this.currentIndex]);
 	}
-	
+
 	getTotalCount(): number {
 		return this.musics.length;
 	}
+
+    resetMusics(): void {
+        this.currentIndex = 0;
+        this.currentMusic.next(this.musics[this.currentIndex]);
+    }
 
 }
