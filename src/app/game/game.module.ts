16d23e2361a7b35ab1e778d7783645d57d310409
Isa <bartkowiak.isadora@gmail.com>
2018-09-21 16:53:19 +0200

    Add component to game module

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MusicService } from './shared/music.service';
+import { TimerService } from './shared/timer.service';
 import { GameContainerComponent } from './game-container/game-container.component';
 import { MusicPlayerComponent } from './music-player/music-player.component';
 import { GameIndicationComponent } from './game-indication/game-indication.component';
+import { IndicationPipe } from './game-indication/indication.pipe';
 
 @NgModule({
   imports: [
@@ -12,9 +14,10 @@ import { GameIndicationComponent } from './game-indication/game-indication.compo
   declarations: [
   	GameContainerComponent,
   	MusicPlayerComponent,
-  	GameIndicationComponent
+  	GameIndicationComponent,
+  	IndicationPipe
   ],
-  providers: [MusicService],
+  providers: [MusicService, TimerService],
   exports: [GameContainerComponent]
 })
 
